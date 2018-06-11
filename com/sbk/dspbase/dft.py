3421e4759ae036aa5086eeb7373a31b7040db04e
SergiySobolev <sergiy.sobolev@gmail.com>
2018-06-11 15:53:50 +0300

    fix i_dft

--- com/sbk/dspbase/dft.py
+++ com/sbk/dspbase/dft.py
@@ -3,29 +3,30 @@ from math import pi, cos, sin
 
 
 def dft(x):
-    # x_len = len(x)
-    # n = x_len // 2 + 1
     n = len(x)
-    re = np.zeros(n)
-    im = np.zeros(n)
-    for i in range(0, n):
+    dft_len = n // 2
+    re = np.zeros(dft_len)
+    im = np.zeros(dft_len)
+    for i in range(0, dft_len):
         for j in range(0, n):
             angle = 2 * pi * i * j / n
             re[i] += x[j] * cos(angle)
-            im[i] += -x[j] * sin(angle)
+            im[i] -= x[j] * sin(angle)
     return re, im
 
+
 def i_dft(re, im):
-    n = len(re)
-    x_len = (n - 1) * 2
-    for i in range(0, n):
-        re[i] = re[i] / (x_len / 2)
-        im[i] = -im[i] / (x_len / 2)
+    dft_len = len(re)
+    n = dft_len * 2 + 1
+    for i in range(0, dft_len):
+        re[i] = re[i] / (n / 2)
+        im[i] = -im[i] / (n / 2)
     re[0] = re[0] / 2
-    re[n - 1] = re[n - 1] / 2
-    x = np.zeros(x_len)
-    for j in range(0, n):
-        for i in range(0, x_len):
-            x[i] += re[j] * cos(2 * pi * i * j / n)
-            x[i] += im[j] * sin(2 * pi * i * j / n)
+    re[-1] = re[-1] / 2
+    x = np.zeros(n)
+    for j in range(0, dft_len):
+        for i in range(0, n):
+            angle = 2 * pi * i * j / n
+            x[i] += re[j] * cos(angle)
+            x[i] += im[j] * sin(angle)
     return x
