5da2760f458c7a94be2f40c17b06e3170c30247f
SergiySobolev <sergiy.sobolev@gmail.com>
2018-06-29 15:04:56 +0300

    delete cache

--- com/sbk/dspbase/pulses/triangle.py
+++ com/sbk/dspbase/pulses/triangle.py
@@ -5,9 +5,11 @@ import numpy as np
 def triangle_pulse(signal_length=512,  peak_height=32, peak_angle_radian=math.pi/4, shift=0):
     t = np.zeros(signal_length)
     b = peak_height * np.arctan(peak_angle_radian)
-    t[int(shift):int(shift+b)] = np.linspace(start=0, stop=peak_height, num=b)
-    ramp_start = int(shift+b)
-    ramp_end = int(shift + 2*b)
-    t[ramp_start:ramp_end] = np.linspace(start=peak_height, stop=0, num=ramp_end-ramp_start)
+    up_ramp_start = np.int64(shift)
+    up_ramp_end = np.int64(shift + b)
+    t[up_ramp_start:up_ramp_end] = np.linspace(start=0, stop=peak_height, num=b)
+    down_ramp_start = np.int64(shift + b)
+    down_ramp_end = np.int64(shift + 2*b)
+    t[down_ramp_start:down_ramp_end] = np.linspace(start=peak_height, stop=0, num=down_ramp_end-down_ramp_start)
     return t
 
