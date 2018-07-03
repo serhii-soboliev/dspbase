7e99fb6bc899ded0ee97fd8d1898a0073ffdd4af
SergiySobolev <sergiy.sobolev@gmail.com>
2018-07-03 09:19:16 +0300

    gaussian burst DFT pair

--- com/sbk/dspbase/pulses/gaussian.py
+++ com/sbk/dspbase/pulses/gaussian.py
@@ -7,8 +7,15 @@ def gaussian_pulse(signal_length=20, mean=0, deviation=1):
     steps_count = 10
     t1 = np.linspace(start=mean - signal_length / 2,
                      stop=mean + signal_length / 2,
-                     num=steps_count * signal_length)
-    a = 1 / (deviation * ((2 * math.pi) ** 1/2))
+                     num=steps_count * signal_length+1)
+    a = 1 / (deviation * ((2 * math.pi) ** 1 / 2))
     b = ((t1 - mean) ** 2) / (2 * deviation)
     t2 = a * np.exp(-b)
     return t1, t2
+
+
+def gaussian_burst(signal_length=20, mean=0, deviation=1, freq=5, amp=2):
+    t1, t2 = gaussian_pulse(signal_length, mean, deviation)
+    s = amp*np.sin(freq*t1)
+    t3 = s * t2
+    return t1, t3
--- tests/pulses/test_gaussian_pulse.py
+++ tests/pulses/test_gaussian_pulse.py
@@ -10,8 +10,21 @@ class GaussianPulseTestCase(unittest.TestCase):
 
     def test_default_gaussian_pulse(self):
         t1, t2 = gaussian_pulse()
-        self.assertEqual(t2.size, 200, "incorrect default gaussian pulse size")
-        self.assertAlmostEqual(t2[99], 0.31790824)
-        self.assertAlmostEqual(t2[100], 0.31790824)
-        self.assertAlmostEqual(t2[101], 0.314713293)
+        self.assertEqual(t2.size, 201, "incorrect default gaussian pulse size")
+        self.assertAlmostEqual(t2[100], 0.3183098861)
+        self.assertAlmostEqual(t2[101], 0.316722309)
+        self.assertAlmostEqual(t2[102], 0.3120069)
 
+    def test_max_must_be_close_to_mean(self):
+        t1, t2 = gaussian_pulse(mean=5)
+        mean_index = t1.tolist().index(5)
+        self.assertAlmostEqual(t2[mean_index], max(t2), msg="Gaussian function have to reach maximum at mean point")
+
+    def test_values_outside_deviation_have_to_be_close_to_zero(self):
+        t1, t2 = gaussian_pulse()
+        out_of_deviation_lower_bound = t1.tolist().index(-1)
+        out_of_deviation_highest_bound = t1.tolist().index(1)
+        self.assertTrue((t2[0:out_of_deviation_lower_bound] > -0.01).all())
+        self.assertTrue((t2[0:out_of_deviation_lower_bound] < 0.5).all())
+        self.assertTrue((t2[out_of_deviation_highest_bound:-1] > -0.01).all())
+        self.assertTrue((t2[out_of_deviation_highest_bound:-1] < 0.5).all())
