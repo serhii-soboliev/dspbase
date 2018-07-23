ae4bd262717a3463c8a63c0c065456087577f721
Serhii Sobolev <sergiy.sobolev@gmail.com>
2018-07-24 02:30:09 +0300

    build_sinus_waves fft

--- com/sbk/dspbase/signals/sinus.py
+++ com/sbk/dspbase/signals/sinus.py
@@ -1,15 +1,14 @@
-from numpy import linspace, zeros, sin, pi
+from numpy import linspace, zeros, sin, pi, arange
 
 
 class Sinus:
 
     @staticmethod
-    def build_sin_waves(sample_rate, signal_length, *args):
+    def build_sin_waves(sample_rate, *args):
         if len(args) % 2 != 0:
             raise ValueError("Parameters sequence length isn't even")
-        sample_num = signal_length*sample_rate
-        t = linspace(start=0, stop=signal_length, num=sample_num)
-        s = zeros(sample_num)
+        t = arange(0, 1, 1.0 / 1000)
+        s = zeros(sample_rate)
         for i in range(0, len(args), 2):
             amplitude = args[i]
             frequency = args[i + 1]
